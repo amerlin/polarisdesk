f587624577e39b85cdeb50131269ec337b236a7f
Andrea Merlin <a.merlin@keantex.com>
2021-01-11 20:01:56 +0100

    ef added

---
+++ src/PolarisDesk.API/Data/PolarisContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PolarisDesk.API.Data
+{
+    public class PolarisContext : DbContext
+    {
+        public PolarisContext(DbContextOptions<PolarisContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
